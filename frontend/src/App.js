9d212c575ebee1be6484bb281a701b3f8e7f9e13
siavash sattari <siavash.sattari.75@gmail.com>
2022-08-14 10:24:16 -0700

    Home screen products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Container } from 'react-bootstrap';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h3>Welcome To ProShop</h3>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
