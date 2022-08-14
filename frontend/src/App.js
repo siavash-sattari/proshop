108c28468f000dc8fa01596ec751e958311b0756
siavash sattari <siavash.sattari.75@gmail.com>
2022-08-14 09:50:03 -0700

    Header, Footer & React Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <h1>Welcome To ProShop</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h3>Welcome To ProShop</h3>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
