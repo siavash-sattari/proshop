3d193c95c59aec587ecbc4a8f65367ecebdf0246
siavash sattari <siavash.sattari.75@gmail.com>
2022-08-17 12:45:05 -0700

    product list reducer, action & HomeScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer
+});
 
 const initialState = {};
 
