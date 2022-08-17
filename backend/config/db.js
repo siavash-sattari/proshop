a0c4ffdb1680b8a59869e234479b427460cc8794
siavash sattari <siavash.sattari.75@gmail.com>
2022-08-17 08:06:54 -0700

    connect to database

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
