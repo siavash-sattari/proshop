61d3f3d93d1bbe135925766d2bfb3efc83d18dbe
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-03 03:51:17 -0700

    Order Screen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -55,7 +55,7 @@ export const getOrderDetails = id => async (dispatch, getState) => {
       }
     };
 
-    const { data } = await axios.get(`/api/orders${id}`, config);
+    const { data } = await axios.get(`/api/orders/${id}`, config)
 
     dispatch({
       type: ORDER_DETAILS_SUCCESS,
